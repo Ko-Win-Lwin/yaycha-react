abf73d72f915559b99dbf62ef1e34e282fe2a53c
Ko-Win-Lwin <winlwin.mce@gmail.com>
2024-08-03 18:40:12 +0630

    Component-props-state

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import List from './components/List.component';
+import Item from './components/Item.component';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [data, setData] = useState([
+    { id: 1, content: "Hello World!", name: "Win Lwin" },
+    { id: 2, content: "React is fun.", name: "Kyi Khant" },
+    { id: 3, content: "Yay, interesting", name: "Zin Bo Khine" }
+  ]);
+
+  const remove = id => {
+    setData(data.filter(item => item.id !== id))
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ maxWidth: 600, margin: "20px auto" }}>
+      <h1>Yaycha</h1>
+      <List>
+        {
+          data.map(item => <Item key={item.id} item={item} remove={remove} />)
+        }
+      </List>
+    </div >
+  );
 }
-
-export default App
---
+++ src/components/List.component.jsx
@@ -0,0 +1,18 @@
+function List({ children }) {
+    return (
+        <ul
+            style={{
+                listStyle: "none",
+                padding: 0,
+                margin: 0,
+                border: "1px solid #ddd",
+                borderRadius: 8,
+                overflow: "hidden",
+            }}>
+            {children}
+        </ul>
+    );
+}
+
+
+export default List;
